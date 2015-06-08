8f7070315fc8e17f9bda938abbaedb8c0b6525c9
Humam Alwassel <haa54@cornell.edu>
2015-06-08 14:32:09 +0100

    changed intedtation to 4 spaces and switched to numpy.linspace instead of the messy nump.arange.

--- basic_methods.py
+++ basic_methods.py
@@ -3,92 +3,95 @@
 
 from __future__ import division
 import numpy as np
-import pylab as pl
+import matplotlib.pyplot as plt
 import math 
+
+debugging = False
 
 # solve the first order ODE y' = f(y, t) on the interval [t0, tf] with 
 # the initial value y(t0) = y0 and time step size h
 def euler_method (f, y0, t0, tf, h):
-  ts = np.arange(t0, tf+h, h)
-  if ts[-1] > tf:
-    ts = np.delete(ts, -1)
-  ys = np.zeros(len(ts), dtype=complex)
-  ys[0] = y0
-  for i in range(len(ts) - 1):
-    ys[i+1] = ys[i] + h*f(ys[i], ts[i])
-  return (ts, ys)
+    assert abs(((tf -t0)/h) % 1) < 1.e-15, "Step size %r does not divide \
+        the interval [%r, %r] evenly." %(h,t0,tf)
+
+    ts = np.linspace(t0, tf, (tf-t0)/h + 1)
+    ys = np.zeros(len(ts), dtype=complex)
+    ys[0] = y0
+
+    for i in range(len(ts) - 1):
+        ys[i+1] = ys[i] + h*f(ys[i], ts[i])
+    return (ts, ys)
 
 # solve the first order ODE y' = f(y, t) on the interval [t0, tf] with 
 # the intial value y(t0) = y0  and time step size h
 def midpoint_method (f, y0, t0, tf, h):
-  ts = np.arange(t0, tf+h, h)
-  if ts[-1] > tf:
-    ts = np.delete(ts, -1)
+    assert abs(((tf -t0)/h) % 1) < 1.e-15, "Step size %r does not divide \
+        the interval [%r, %r] evenly." %(h,t0,tf)
+    ts = np.linspace(t0, tf, (tf-t0)/h + 1)
+    ys = np.zeros(len(ts), dtype=complex)
+    ys[0] = y0
+    ys[1] = ys[0] + h*f(ys[0], ts[0]) # use Euler to get second initial value
+    for i in range(len(ts) - 2):
+        ys[i+2] = ys[i] + 2*h*f(ys[i+1], ts[i+1])
+    return (ts, ys)
 
-  ys = np.zeros(len(ts), dtype=complex)
-  ys[0] = y0
-  ys[1] = ys[0] + h*f(ys[0], ts[0]) # use Euler to get second initial value
-  for i in range(len(ts) - 2):
-    ys[i+2] = ys[i] + 2*h*f(ys[i+1], ts[i+1])
-  return (ts, ys)
-
-# preforms a convergence test solving the IVP y' = f(y) and y(0) = y0 with
+# preforms a convergence test solving the IVP y' = f(y) and y(t0) = y0 with
 # the [method] provided, where the function exact is the solution the IVP.
 # The function plots the ||error|| vs time step size, and a reference line
 # of the given order to compare. The graph is titled with graph_title
 def convergence_test(f, exact, t0, tf, y0, method, order, graph_title):
-  hs = np.asarray([2**(-k) for k in range(1, 14)])
-  err = np.zeros(len(hs))
+    hs = np.asarray([2**(-k) for k in range(1, 16)])
+    err = np.zeros(len(hs))
 
-  for i in range(len(hs)):
-    ts, ys = method(f, y0, t0, tf, hs[i])
-    err[i] = abs(ys[-1] - exact(tf))
+    for i in range(len(hs)):
+        ts, ys = method(f, y0, t0, tf, hs[i])
+        err[i] = abs(ys[-1] - exact(tf))
 
-  pl.hold('true')
-  method_err,  = pl.loglog(hs, err, 's-')
-  order_line,  = pl.loglog(hs, (hs**order)*(err[5]/hs[5]))
-  pl.legend([method_err, order_line], ['Method Error', 'order ' + str(order)], loc=2)
-  pl.title(graph_title)
-  pl.ylabel('||error||')
-  pl.xlabel('time step size')
+    plt.hold('true')
+    method_err,  = plt.loglog(hs, err, 's-')
+    order_line,  = plt.loglog(hs, (hs**order)*(err[5]/hs[5]))
+    plt.legend([method_err, order_line], ['Method Error', 'order ' + str(order)], loc=2)
+    plt.title(graph_title)
+    plt.ylabel('||error||')
+    plt.xlabel('time step size')
 
-  pl.show()
+    plt.show()
 
 # Convergence Tests 
+if debugging:
+    #### test 1 ####
+    lam = -2
+    f = lambda y,t: lam*y
+    y0 = 1
+    exact = lambda t: y0*np.exp(lam*t)
+    t0 = 1
+    tf = 5
+    convergence_test(f, exact, t0, tf, exact(t0), euler_method, 1, "euler_method")
+    convergence_test(f, exact, t0, tf, exact(t0), midpoint_method, 2, "midpoint_method")
 
-#### test 1 ####
-lam = -1j
-f = lambda y,t: lam*y
-y0 = 1
-exact = lambda t: y0*np.exp(lam*t)
-t0 = 0
-tf = 10
-convergence_test(f, exact, t0, tf, exact(t0), euler_method, 1, "euler_method")
-convergence_test(f, exact, t0, tf, exact(t0), midpoint_method, 2, "midpoint_method")
+    ### test 2 ####
+    f = lambda y,t: 4.*y*float(np.sin(t))**3*np.cos(t)
+    y0 = 1
+    exact = lambda t: y0*np.exp((np.sin(t))**4)
+    t0 = 0
+    tf = 5
+    convergence_test(f, exact, t0, tf, exact(t0), euler_method, 1, "euler_method")
+    convergence_test(f, exact, t0, tf, exact(t0), midpoint_method, 2, "midpoint_method")
 
-# #### test 2 ####
-f = lambda y,t: 4.*y*float(np.sin(t))**3*np.cos(t)
-y0 = 1
-exact = lambda t: y0*np.exp((np.sin(t))**4)
-t0 = 0
-tf = 5
-convergence_test(f, exact, t0, tf, exact(t0), euler_method, 1, "euler_method")
-convergence_test(f, exact, t0, tf, exact(t0), midpoint_method, 2, "midpoint_method")
+    #### test 3 ####
+    f = lambda y,t: 4.*t*np.sqrt(y)
+    y0 = 1
+    exact = lambda t: (1.+t**2)**2
+    t0 = 0
+    tf = 5
+    convergence_test(f, exact, t0, tf, exact(t0), euler_method, 1, "euler_method")
+    convergence_test(f, exact, t0, tf, exact(t0), midpoint_method, 2, "midpoint_method")
 
-# #### test 3 ####
-f = lambda y,t: 4.*t*np.sqrt(y)
-y0 = 1
-exact = lambda t: (1.+t**2)**2
-t0 = 0
-tf = 5
-convergence_test(f, exact, t0, tf, exact(t0), euler_method, 1, "euler_method")
-convergence_test(f, exact, t0, tf, exact(t0), midpoint_method, 2, "midpoint_method")
-
-#### test 4 ####
-f = lambda y,t:  y/t*np.log(y)
-y0 = np.exp(1.)
-exact = lambda t: np.exp(2.*t)
-t0 = 0.5
-tf = 5
-convergence_test(f, exact, t0, tf, exact(t0), euler_method, 1, "euler_method")
-convergence_test(f, exact, t0, tf, exact(t0), midpoint_method, 2, "midpoint_method")
+    #### test 4 ####
+    f = lambda y,t:  y/t*np.log(y)
+    y0 = np.exp(1.)
+    exact = lambda t: np.exp(2.*t)
+    t0 = 0.5
+    tf = 5
+    convergence_test(f, exact, t0, tf, exact(t0), euler_method, 1, "euler_method")
+    convergence_test(f, exact, t0, tf, exact(t0), midpoint_method, 2, "midpoint_method")
--- ex_euler_serial.py
+++ ex_euler_serial.py
@@ -1,114 +1,112 @@
 
 from __future__ import division
 import numpy as np
-import pylab as pl
+import matplotlib.pyplot as plt
 import math 
 
+debugging = True
+
 def ex_one_step (f, yn, tn, h, p):
-  Y = np.zeros((p+1,p+1), dtype=complex)
-  T = np.zeros((p+1,p+1), dtype=complex)
+    Y = np.zeros((p+1,p+1), dtype=complex)
+    T = np.zeros((p+1,p+1), dtype=complex)
   
-  for k in range(1,p+1):
-    Y[k,0] = yn
-    for j in range(1,k+1):
-      Y[k,j] = Y[k,j-1] + (h/k)*f(Y[k,j-1], tn + j*(h/k))
-    T[k,1] = Y[k,k]
+    for k in range(1,p+1):
+        Y[k,0] = yn
+        for j in range(1,k+1):
+            Y[k,j] = Y[k,j-1] + (h/k)*f(Y[k,j-1], tn + j*(h/k))
+        T[k,1] = Y[k,k]
 
-  for k in range(2, p+1):
-    for j in range(k, p+1):
-      T[j,k] = T[j,k-1] + (T[j,k-1] - T[j-1,k-1])/((j/(j-k+1)) - 1)
+    for k in range(2, p+1):
+        for j in range(k, p+1):
+            T[j,k] = T[j,k-1] + (T[j,k-1] - T[j-1,k-1])/((j/(j-k+1)) - 1)
 
-  return (T[p,p], T[p-1,p-1])
+    return (T[p,p], T[p-1,p-1])
 
 # solve the first order ODE y' = f(y) by extrapolation based on Euler's method
 # on the interval [t0, tf] with the intial value y(t0) = y0 and time step size
 # h and order p
 def ex_euler_serial (f, y0, t0, tf, h, p):
-  ts = np.arange(t0, tf+h, h)
-  if ts[-1] > tf:
-    ts = np.delete(ts, -1)
-  ys = np.zeros(len(ts), dtype=complex)
-  ys_hat = np.zeros(len(ts), dtype=complex)
-  ys[0] = y0
+    assert abs(((tf -t0)/h) % 1) < 1.e-15, "Step size %r does not divide \
+        the interval [%r, %r] evenly." %(h,t0,tf)
+    ts = np.linspace(t0, tf, (tf-t0)/h + 1)
+    ys = np.zeros(len(ts), dtype=complex)
+    ys_hat = np.zeros(len(ts), dtype=complex)
+    ys[0] = y0
 
-  for i in range(len(ts) - 1):
-    ys[i+1], ys_hat[i+1] = ex_one_step(f, ys[i], ts[i], h, p)
+    for i in range(len(ts) - 1):
+        ys[i+1], ys_hat[i+1] = ex_one_step(f, ys[i], ts[i], h, p)
 
-  return (ts, ys, ys_hat)
+    return (ts, ys, ys_hat)
 
-
-# preforms a convergence test solving the IVP y' = f(y) and y(0) = y0 with
+# preforms a convergence test solving the IVP y' = f(y) and y(t0) = y0 with
 # the [method] provided, where the function exact is the solution the IVP.
 # The function plots the ||error|| vs time step size, and a reference line
 # of the given order to compare. The graph is titled with graph_title.
 def convergence_test(f, exact, t0, tf, y0, method, order, graph_title):
-  hs = np.asarray([2**(-k) for k in range(1, 10)])
-  err = np.zeros(len(hs))
+    hs = np.asarray([2**(-k) for k in range(1, 10)])
+    err = np.zeros(len(hs))
 
-  for i in range(len(hs)):
-    ts, ys, _ = method(f, y0, t0, tf, hs[i], order)
-    err[i] = abs(ys[-1] - exact(tf))
+    for i in range(len(hs)):
+        ts, ys, _ = method(f, y0, t0, tf, hs[i], order)
+        err[i] = abs(ys[-1] - exact(tf))
 
-  pl.hold('true')
-  method_err,  = pl.loglog(hs, err, 's-')
-  order_line,  = pl.loglog(hs, (hs**order)*(err[5]/hs[5]))
-  pl.legend([method_err, order_line], ['Method Error', 'order ' + str(order)], loc=2)
-  pl.title(graph_title)
-  pl.ylabel('||error||')
-  pl.xlabel('time step size')
-  pl.show()
+    plt.hold('true')
+    method_err,  = plt.loglog(hs, err, 's-')
+    order_line,  = plt.loglog(hs, (hs**order)*(err[5]/hs[5]))
+    plt.legend([method_err, order_line], ['Method Error', 'order ' + str(order)], loc=2)
+    plt.title(graph_title)
+    plt.ylabel('||error||')
+    plt.xlabel('time step size')
+    plt.show()
 
+# Convergence Tests
+if debugging:
+    #### test 1 ####
+    lam = -1j
+    y0 = 1
+    f = lambda y,t: lam*y
+    exact = lambda t: y0*np.exp(lam*t)
+    t0 = 0
+    tf = 10
+    convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 1, "ex_euler_serial")
+    convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 2, "ex_euler_serial")
+    convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 3, "ex_euler_serial")
+    convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 4, "ex_euler_serial")
+    convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 5, "ex_euler_serial")
+    convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 6, "ex_euler_serial")
 
-# Convergence Test 
+    #### test 2 ####
+    f = lambda y,t: 4.*y*float(np.sin(t))**3*np.cos(t)
+    y0 = 1
+    exact = lambda t: y0*np.exp((np.sin(t))**4)
+    t0 = 0
+    tf = 10
+    convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 1, "ex_euler_serial")
+    convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 2, "ex_euler_serial")
+    convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 3, "ex_euler_serial")
+    convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 4, "ex_euler_serial")
+    convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 5, "ex_euler_serial")
 
-#### test 1 ####
-lam = -1j
-y0 = 1
-f = lambda y,t: lam*y
-exact = lambda t: y0*np.exp(lam*t)
+    #### test 3 ####
+    f = lambda y,t: 4.*t*np.sqrt(y)
+    y0 = 1
+    exact = lambda t: (1.+t**2)**2
+    t0 = 0
+    tf = 10
+    convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 1, "ex_euler_serial")
+    convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 2, "ex_euler_serial")
+    convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 3, "ex_euler_serial")
+    convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 4, "ex_euler_serial")
+    convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 5, "ex_euler_serial")
 
-t0 = 0
-tf = 10
-convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 1, "ex_euler_serial")
-convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 2, "ex_euler_serial")
-convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 3, "ex_euler_serial")
-convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 4, "ex_euler_serial")
-convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 5, "ex_euler_serial")
-convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 6, "ex_euler_serial")
-
-
-# #### test 2 ####
-f = lambda y,t: 4.*y*float(np.sin(t))**3*np.cos(t)
-y0 = 1
-exact = lambda t: y0*np.exp((np.sin(t))**4)
-t0 = 0
-tf = 10
-convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 1, "ex_euler_serial")
-convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 2, "ex_euler_serial")
-convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 3, "ex_euler_serial")
-convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 4, "ex_euler_serial")
-convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 5, "ex_euler_serial")
-
-# #### test 3 ####
-f = lambda y,t: 4.*t*np.sqrt(y)
-y0 = 1
-exact = lambda t: (1.+t**2)**2
-t0 = 0
-tf = 10
-convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 1, "ex_euler_serial")
-convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 2, "ex_euler_serial")
-convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 3, "ex_euler_serial")
-convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 4, "ex_euler_serial")
-convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 5, "ex_euler_serial")
-
-#### test 4 ####
-f = lambda y,t:  y/t*np.log(y)
-y0 = np.exp(1.)
-exact = lambda t: np.exp(2.*t)
-t0 = 0.5
-tf = 10
-convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 1, "ex_euler_serial")
-convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 2, "ex_euler_serial")
-convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 3, "ex_euler_serial")
-convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 4, "ex_euler_serial")
-convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 5, "ex_euler_serial")
+    #### test 4 ####
+    f = lambda y,t:  y/t*np.log(y)
+    y0 = np.exp(1.)
+    exact = lambda t: np.exp(2.*t)
+    t0 = 0.5
+    tf = 10
+    convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 1, "ex_euler_serial")
+    convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 2, "ex_euler_serial")
+    convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 3, "ex_euler_serial")
+    convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 4, "ex_euler_serial")
+    convergence_test(f, exact, t0, tf, exact(t0), ex_euler_serial, 5, "ex_euler_serial")
